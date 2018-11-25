a6973eb8d02a05fe5e40f25c5c16586259a8f85c
MQasimSarfraz <syed.qasim.sarfraz@gmail.com>
2018-11-25 22:52:33 +0000

    Update resume

--- resume/build.sh
+++ resume/build.sh
@@ -4,11 +4,12 @@ INPUT="resume.tex"
 OUTPUT="resume.pdf"
 DOCKER_IMAGE="blang/latex:ubuntu"
 
-echo "====>: Creating ${OUTPUT} from ${INPUT}"
 
-docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "${PWD}":/data ${DOCKER_IMAGE} pdflatex ${INPUT} > /dev/null 2>&1
-if [[ $? -ne 0 ]]; then
-  echo "Error when creaing pdf, check resume.log for details"
-fi
+echo "====>: Pulling ${DOCKER_IMAGE} ..."
+docker pull ${DOCKER_IMAGE}
+
+echo "====>: Creating ${OUTPUT} from ${INPUT} ..."
+
+docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "${PWD}":/data ${DOCKER_IMAGE} pdflatex ${INPUT} > /dev/null
 
 echo "====>: Finished creating ${OUTPUT}"
