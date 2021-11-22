62a434f05e3539ef9615b7d0c3571cae7fa8aed8
Qasim Sarfraz <qasim.sarfraz@esailors.de>
2021-11-22 17:10:41 +0000

    Fix resume.pdf url

--- resume/build.sh
+++ resume/build.sh
@@ -2,6 +2,7 @@
 
 INPUT="resume.tex"
 OUTPUT="resume.pdf"
+PORTFOLIO_PATH="../static/resume"
 DOCKER_IMAGE="blang/latex:ubuntu"
 
 
@@ -11,5 +12,9 @@ docker pull ${DOCKER_IMAGE}
 echo "====>: Creating ${OUTPUT} from ${INPUT} ..."
 
 docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "${PWD}":/data ${DOCKER_IMAGE} pdflatex ${INPUT} > /dev/null
+
+echo "====>: Copying ${INPUT} to ${PORTFOLIO_PATH}"
+mkdir -p ${PORTFOLIO_PATH}
+cp resume.pdf ${PORTFOLIO_PATH}
 
 echo "====>: Finished creating ${OUTPUT}"
